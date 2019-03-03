a2e259299ac4f3de0f2fad94e1fba40890a6c687
pinkpingpig <pinkpingpig@LAPTOP-6M9BOEJ8>
2019-03-03 10:40:47 +0800

    第一次构建项目

---
+++ src/main/java/com/chucloud/BrainStrom/BrainStromApplication.java
@@ -0,0 +1,13 @@
+package com.chucloud.BrainStrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainStromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainStromApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chucloud/BrainStrom/BrainStromApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chucloud.BrainStrom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrainStromApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
